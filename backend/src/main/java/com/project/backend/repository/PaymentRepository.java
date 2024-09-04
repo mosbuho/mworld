04342372256c2f03998b07208608d1987a24bc3c
Molae <jwk3497@gmail.com>
2024-09-04 15:54:30 +0900

    product 엔티티, 레파지토리 기본키타입 int 로 변경

--- backend/src/main/java/com/project/backend/repository/PaymentRepository.java
+++ backend/src/main/java/com/project/backend/repository/PaymentRepository.java
@@ -4,6 +4,6 @@ import com.project.backend.entity.Payment;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 
-public interface PaymentRepository extends JpaRepository<Payment, Long> {
+public interface PaymentRepository extends JpaRepository<Payment, Integer> {
 
 }
