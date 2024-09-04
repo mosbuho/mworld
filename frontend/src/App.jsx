9ebf64345ba846875473a1f6572344d6a51bd3cc
Lee-1sj <sukjin0112@naver.com>
2024-09-04 21:20:40 +0900

    소셜 로그인 작업

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,6 +3,9 @@ import PrivateRoute from './components/common/PrivateRoute';
 import PublicRoute from './components/common/PublicRoute';
 import './App.css'
 
+import MemberLogin from './pages/member/MemberLogin';
+import OAuthCallback from './utils/OAuthCallback';
+
 function App() {
 
   return (
@@ -10,11 +13,13 @@ function App() {
       <Routes>
         {/* 공개 라우트 */}
         {/* <Route path="/" element={<MemberMain />} /> */}
-        
+
         {/* 비로그인 라우트 */}
-        <Route path="/login" element={<PublicRoute><MemberLogin/></PublicRoute>} />
+        <Route path="/login" element={<PublicRoute><MemberLogin /></PublicRoute>} />
         {/* <Route path="/admin/login" element={<PublicRoute><AdminLogin /></PublicRoute>} /> */}
 
+        {/* OAuth 콜백 처리 경로 */}
+        <Route path="/oauth/callback/:provider" element={<OAuthCallback/>} />
       </Routes>
     </BrowserRouter>
   )
