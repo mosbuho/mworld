f6bcaa266ef00fbe8f75580b145c3378cc4caefd
Molae <jwk3497@gmail.com>
2024-09-05 11:35:50 +0900

    회원리스트페이지(검색어와 기간검색 동시에 했을때)오류처리

--- backend/src/main/java/com/project/backend/repository/MemberRepository.java
+++ backend/src/main/java/com/project/backend/repository/MemberRepository.java
@@ -8,21 +8,23 @@ import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 
 import java.time.LocalDate;
+import java.time.LocalDateTime;
 
 public interface MemberRepository extends JpaRepository<Member, Integer> {
     Member findMemberById(String id);
 
     @Query("SELECT m FROM Member m WHERE " +
-            "(:f = 'ID' AND m.id LIKE %:q%) OR " +
+            "((:f = 'ID' AND m.id LIKE %:q%) OR " +
             "(:f = 'NAME' AND m.name LIKE %:q%) OR " +
-            "(:f = 'PHONE' AND m.phone LIKE %:q%) " +
-            "AND (:startDate IS NULL OR m.regDate >= :startDate) " +
-            "AND (:endDate IS NULL OR m.regDate <= :endDate)")
+            "(:f = 'PHONE' AND m.phone LIKE %:q%) OR " +
+            "(:q IS NULL OR :q = '')) AND " + // 검색어가 없을 때는 이 조건 무시
+            "((:startDate IS NULL OR m.regDate >= :startDate) AND " +
+            "(:endDate IS NULL OR m.regDate <= :endDate))")  // 기간이 없을 때는 이 조건 무시
     Page<Member> findByField(
             @Param("f") String f,
             @Param("q") String q,
-            @Param("startDate") LocalDate startDate,
-            @Param("endDate") LocalDate endDate,
+            @Param("startDate") LocalDateTime startDate,
+            @Param("endDate") LocalDateTime endDate,
             Pageable pageable
     );
 
