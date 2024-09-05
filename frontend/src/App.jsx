ab62c9023178ba7c4c944e4249a11880fb449ef2
chltjdfkr517@naver.com <chltjdfkr517@naver.com>
2024-09-05 22:54:12 +0900

    로그인, 회원가입 프론트 구현

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,10 +1,25 @@
 import './App.css'
+import { Routes, Route } from 'react-router-dom';
+import Login from './components/Login';
+import AdminLogin from './pages/AdminLogin';
+import SignUp from './pages/SignUp';
+
 
 function App() {
 
   return (
-    <>
-    </>
+    <div className='App'>
+      <Routes>
+        {/* 메인 및 다른 경로 설정 */}
+        {/* <Route path='/*' element={<Main />} /> */}
+        {/* <Route path='/admin/*' element={<Admin />} /> */}
+
+        <Route path='/login' element={<Login />} />
+        <Route path='/admin/login' element={<AdminLogin />} />
+        <Route path='/signup' element={<SignUp />} />
+      </Routes>
+
+    </div>
   )
 }
 
