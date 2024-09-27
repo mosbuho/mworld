3160786ffd2b8317aa27f448c0d31ed6c2a02591
Molae <jwk3497@gmail.com>
2024-09-28 01:39:30 +0900

    어드민 상품관리(update,delete) 작업완료

--- backend/src/main/java/com/project/backend/controller/ProductController.java
+++ backend/src/main/java/com/project/backend/controller/ProductController.java
@@ -6,10 +6,9 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Map;
 
 @RestController
 @RequestMapping("/api")
@@ -24,14 +23,47 @@ public class ProductController {
 
     @PostMapping("/admin/product")
     public ResponseEntity<String> createProduct(@RequestBody Product product) {
-        try{
+        try {
             log.info("createProduct : " + product);
             Product savedProduct = productService.createProduct(product);
             log.info("savedProduct : " + savedProduct);
             return ResponseEntity.ok(savedProduct.getTitle());
-        }catch (Exception e){
+        } catch (Exception e) {
             e.printStackTrace();
             return ResponseEntity.badRequest().build();
+        }
+    }
+
+    @GetMapping("/admin/product-list")
+    public Map<String, Object> getAllProducts(
+            @RequestParam(defaultValue = "1") int page,
+            @RequestParam(defaultValue = "20") int size,
+            @RequestParam String f,
+            @RequestParam String q) {
+        return productService.getProductWithPagination(page, size, f, q);
+    }
+
+    @PutMapping("/admin/product/{no}")
+    public ResponseEntity<String> updateProduct(
+            @PathVariable int no,
+            @RequestBody Product updateProduct) {
+        try {
+            log.info("updateProduct : " + updateProduct);
+            productService.updateProduct(no, updateProduct);
+            return ResponseEntity.ok(null);
+        } catch (Exception e) {
+            return ResponseEntity.status(500).build();
+        }
+    }
+
+    @DeleteMapping("/admin/product/{no}")
+    public ResponseEntity<String> deleteProduct(
+            @PathVariable int no) {
+        try {
+            productService.deleteProduct(no);
+            return ResponseEntity.ok(null);
+        } catch (Exception e) {
+            return ResponseEntity.status(500).build();
         }
     }
 }
