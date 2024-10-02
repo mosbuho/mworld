6768224d8e109b5e4b063b39ed904731c89dd370
chltjdfkr517@naver.com <chltjdfkr517@naver.com>
2024-10-02 16:40:38 +0900

    마이페이지 내 정보 : 정보변경 기능 추가

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,20 +1,19 @@
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import './App.css';
 import PublicRoute from './components/common/PublicRoute';
+import OAuthCallback from './utils/OAuthCallback';
+import SignUp from './pages/SignUp';
+
+import MemberMain from './pages/member/MemberMain.jsx';
+import MemberLogin from './pages/member/MemberLogin';
+import MainProduct from './components/member/MainProduct.jsx';
+import ProductDetail from './pages/member/ProductDetail';
+import MyPage from './pages/member/MyPage';
+
 import AdminMain from "./pages/admin/AdminMain.jsx";
 import AdminMember from "./pages/admin/AdminMember.jsx";
 import AdminMemberList from "./pages/admin/AdminMemberList.jsx";
 import AdminLogin from './pages/AdminLogin';
-import SignUp from './pages/SignUp';
-import MemberMain from './pages/member/MemberMain.jsx';
-import MyPage from './pages/member/MyPage.jsx';
-
-import MemberLogin from './pages/member/MemberLogin';
-import OAuthCallback from './utils/OAuthCallback';
-
-import ProductDetail from './pages/member/ProductDetail.jsx';
-import MainProduct from './components/member/MainProduct.jsx';
-
 import AdminProductList from "./pages/admin/AdminProductList.jsx";
 import AdminProductCreate from "./pages/admin/AdminProductCreate.jsx";
 import AdminProduct from "./pages/admin/AdminProduct.jsx";
