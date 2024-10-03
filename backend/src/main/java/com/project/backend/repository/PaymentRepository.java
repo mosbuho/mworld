d94ec6bf350a5d28121e4e105531639cbb37fa9a
Molae <jwk3497@gmail.com>
2024-10-04 08:38:44 +0900

    백엔드 Payment엔티티수정(method, status 컬럼 추가) 및 주문내역 로직 수정 PaymentResponse DTO 생성.

--- backend/src/main/java/com/project/backend/repository/PaymentRepository.java
+++ backend/src/main/java/com/project/backend/repository/PaymentRepository.java
@@ -1,9 +1,18 @@
 package com.project.backend.repository;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.project.backend.entity.Payment;
+import org.springframework.data.jpa.repository.Query;
 
 public interface PaymentRepository extends JpaRepository<Payment, Integer> {
 
+    @Query("SELECT p.transactionId, p.method, SUM(p.price), MAX(p.regDate), p.status, m.name, m.phone " +
+            "FROM Payment p " +
+            "JOIN p.member m " +
+            "GROUP BY p.transactionId, p.method, p.status, m.name, m.phone " +
+            "ORDER BY MAX(p.regDate) DESC")
+    Page<Object[]> findGroupedPayments(Pageable pageable);
 }
